4fce19b66ae15fab89d57006c14d05b47ceb3998
danielnowendsztern <danielnowens@hotmail.com>
2014-03-30 09:55:43 +0100

    librerias agregadas

--- src/cup/alex/UnidadLexica.java
+++ src/cup/alex/UnidadLexica.java
@@ -3,12 +3,12 @@ package cup.alex;
 import java_cup.runtime.Symbol;
 
 public class UnidadLexica extends Symbol {
-   private int fila;
-   public UnidadLexica(int fila, int clase, String lexema) {
-     super(clase,lexema);
-	 this.fila = fila;
-   }
-   public int clase () {return sym;}
-   public String lexema() {return (String)value;}
-   public int fila() {return fila;}
+	private int fila;
+	public UnidadLexica(int fila, int clase, String lexema) {
+		super(clase,lexema);
+		this.fila = fila;
+	}
+	public int clase () {return sym;}
+	public String lexema() {return (String)value;}
+	public int fila() {return fila;}
 }
